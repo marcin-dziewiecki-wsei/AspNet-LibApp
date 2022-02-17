6f2427ecee48881888b1d9f1fdf60dce04ddeb6a
Marcin Dziewięcki <marcin@dziewiecki.net>
2022-02-17 22:51:08 +0100

    Refactor Database Seed Method

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using LibApp.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace LibApp.Data
@@ -15,6 +18,57 @@ namespace LibApp.Data
                 await context.Database.MigrateAsync();
             }
             return app;
+        }
+
+        public static async Task<IHost> UseDatabaseSeed(this IHost app)
+        {
+            using (var scope = app.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                await SeedMembershipTypes(context);
+            }
+            return app;
+        }
+
+        private static async Task SeedMembershipTypes(ApplicationDbContext context)
+        {
+            if (context.MembershipTypes.Any())
+            {
+                Console.WriteLine("MembershipTypes already seeded");
+                return;
+            }
+
+            context.MembershipTypes.AddRange(
+                new MembershipType
+                {
+                    Id = 1,
+                    SignUpFee = 0,
+                    DurationInMonths = 0,
+                    DiscountRate = 0
+                },
+                new MembershipType
+                {
+                    Id = 2,
+                    SignUpFee = 30,
+                    DurationInMonths = 1,
+                    DiscountRate = 10
+                },
+                new MembershipType
+                {
+                    Id = 3,
+                    SignUpFee = 90,
+                    DurationInMonths = 3,
+                    DiscountRate = 15
+                },
+                new MembershipType
+                {
+                    Id = 4,
+                    SignUpFee = 300,
+                    DurationInMonths = 12,
+                    DiscountRate = 20
+                });
+
+            await context.SaveChangesAsync();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,14 +1,7 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using LibApp.Models;
-using Microsoft.Extensions.DependencyInjection;
 using LibApp.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
 
 namespace LibApp
 {
@@ -19,13 +12,7 @@ namespace LibApp
             var host = CreateHostBuilder(args).Build();
 
             await host.UseDatabaseAutoMigration();
-
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-
-                SeedData.Initialize(services);
-            }
+            await host.UseDatabaseSeed();
 
             host.Run();
         }
