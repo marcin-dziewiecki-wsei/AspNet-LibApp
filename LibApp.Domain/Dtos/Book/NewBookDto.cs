8104e3b416dc674416f8b012183359c6d108525d
Marcin Dziewięcki <marcin@dziewiecki.net>
2022-02-20 16:56:26 +0100

    Add validation on book Dto models

--- LibApp.Domain/Dtos/Book/NewBookDto.cs
+++ LibApp.Domain/Dtos/Book/NewBookDto.cs
@@ -1,13 +1,28 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace LibApp.Domain.Dtos.Book
 {
     public class NewBookDto
     {
+		[Required(AllowEmptyStrings = false, ErrorMessage = "Book name must be set")]
+		[StringLength(50, MinimumLength = 5, ErrorMessage = "Book name must be between 5 and 50 characters long")]
 		public string Name { get; set; }
+
+		[Required(AllowEmptyStrings = false, ErrorMessage = "Author name must be set")]
+		[StringLength(50, MinimumLength = 5, ErrorMessage = "Author name must be between 5 and 50 characters long")]
 		public string AuthorName { get; set; }
+
+		[Required(ErrorMessage = "Genere must be set")]
+		[Range(1, 255, ErrorMessage = "Genere must be properly set")]
 		public byte GenreId { get; set; }
+		
+		[Required(ErrorMessage = "Release date must be set")]
+		[DataType(DataType.Date, ErrorMessage = "Release date must be proper date")]
 		public DateTime ReleaseDate { get; set; }
+
+		[Required(ErrorMessage = "Number of books in stock must be set")]
+		[Range(1,20, ErrorMessage = "Number of books in stock must be betwen 1 and 20")]
 		public int NumberInStock { get; set; }
 	}
 }
--- LibApp.Domain/Dtos/Book/UpdateBookDto.cs
+++ LibApp.Domain/Dtos/Book/UpdateBookDto.cs
@@ -1,7 +1,11 @@
-﻿namespace LibApp.Domain.Dtos.Book
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibApp.Domain.Dtos.Book
 {
     public class UpdateBookDto: NewBookDto
     {
+        [Required(ErrorMessage = "Book id must be set")]
+        [Range(1, int.MaxValue, ErrorMessage = "Value must be positive value")]
         public int Id { get; set; }
     }
 }
